79ae784c05726675627557dc7e7ca5f8d59818b2
shyammohan123 <shyammohan123@gmail.com>
2022-11-21 10:15:40 +0530

    Login Functionality Shibi

--- src/org/gittesting/GitExample.java
+++ src/org/gittesting/GitExample.java
@@ -1,6 +1,6 @@
 package org.gittesting;
 
-
+import org.pack.Company;
 
 public class GitExample {
 
@@ -11,5 +11,35 @@ public class GitExample {
 		
 
 	}
+
+}
+
+Company(){
+	super(1991,31);
+
+	System.out.println("Default Constructor");
+
+}
+
+Company(int id){
+	super(2431267);
+	System.out.println("id :"+id);
+}
+
+	Company(String name){
+
+		System.out.println("Company name :"+name);
+
+	}
+	public static void main(String[] args) {
+		Company c = new Company("Mphasis");
+		Company c1 = new Company();
+		Company c2 = new Company(1234);
+
+
+
+
+	}
+
 
 }
